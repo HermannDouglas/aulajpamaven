7335e7d91da641d8c1a8707d535b5fc658835989
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-10-05 15:41:28 -0500

    Nivelamento JPA

--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -2,10 +2,17 @@ package dominio;
 
 import java.io.Serializable;
 
-public class Pessoa implements Serializable{
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
-	
+@Entity
+public class Pessoa implements Serializable{
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String email;
